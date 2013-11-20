d2d07895af2f4f89b18467567ae45f6af0e5d624
Oren Leiman <oleiman@ucsc.edu>
2013-11-20 05:56:15 -0800

    Move some files around
    Signed-off-by: Oren Leiman <oleiman@ucsc.edu>

--- src/cls_llvmpy/setup.py
+++ src/cls_llvmpy/setup.py
@@ -1,6 +1,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
+import os
  
+try:
+    os.chdir('./src/cls_llvmpy/')
+finally:
+    pass
+
 setup(name="llvmpy-array",
     ext_modules=[
         Extension("llvm_client", ["cls_llvmpy_client.cc", "cls_llvm_ops.cc"],
